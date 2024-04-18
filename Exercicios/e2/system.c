d515ff1d0a85feff4b8697050fd88855887d64af
anapaulacostacurta08 <75001497+anapaulacostacurta08@users.noreply.github.com>
2024-04-18 18:13:06 -0300

    Update system.c

--- Exercicios/e2/system.c
+++ Exercicios/e2/system.c
@@ -1,3 +1,10 @@
+/********************************************************
+* Faz parte da biblioteca padrão do C                   *
+* Fácil de usar                                         *
+* Cria um subprocesso rodando o shell padrão e passa o  *
+* comando ao novo shell para execução
+*********************************************************/
+
 #include <stdlib.h>
 
 int main ()
